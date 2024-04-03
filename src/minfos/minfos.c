7d9463ba8019f3d105b4c0e45cbd641a4e5f4e9c
zom <contact.zomzd@gmail.com>
2024-04-03 16:08:10 +0200

    Parsing Redirections 2/2

--- src/minfos/minfos.c
+++ src/minfos/minfos.c
@@ -6,7 +6,7 @@
 /*   By: truello <truello@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 14:09:35 by truello           #+#    #+#             */
-/*   Updated: 2024/04/03 16:04:12 by truello          ###   ########.fr       */
+/*   Updated: 2024/04/03 16:07:26 by truello          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,8 @@ void	clear_minfos_rfiles(t_minishell *minfos)
 void	free_minfos(t_minishell *minfos)
 {
 	clear_minfos_rfiles(minfos);
-	if (!minfos->env)
+	if (minfos->env)
 		free_parts(minfos->env);
-	if (!minfos)
+	if (minfos)
 		free(minfos);
 }
