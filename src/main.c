13452f431c4015b085dbdd33e22b23f2e69c45f4
zom <contact.zomzd@gmail.com>
2024-04-16 14:44:54 +0200

    Ajout de la condition d arret quand la commande est nulle

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: truello <truello@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 11:29:46 by truello           #+#    #+#             */
-/*   Updated: 2024/04/15 14:53:05 by truello          ###   ########.fr       */
+/*   Updated: 2024/04/16 14:44:24 by truello          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	main(void)
 	char	*line;
 
 	line = readline("minishell > ");
-	while (line)
+	while (line && line[0] != '\0')
 	{
 		parse_line(line);
 		free(line);
