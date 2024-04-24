a305ab2188046ea2b1dab2f595d1fe3e0c608fa4
ZomDev3343 <contact.zomzd@gmail.com>
2024-04-24 18:11:23 +0200

    clean code

--- src/parsing/quotes.c
+++ src/parsing/quotes.c
@@ -6,7 +6,7 @@
 /*   By: tohma <tohma@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 20:02:09 by tohma             #+#    #+#             */
-/*   Updated: 2024/04/24 12:55:44 by tohma            ###   ########.fr       */
+/*   Updated: 2024/04/24 18:10:52 by tohma            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,46 +28,6 @@ int	is_quote_closed(char *str)
 			return (i);
 	return (0);
 }
-
-static int	rem_double_quotes(t_string_part **parts, char *str, t_env *env)
-{
-	int	end_quote;
-	int	i;
-	int	last_cpy_index;
-
-	end_quote = is_quote_closed(str);
-	if (!end_quote)
-		return (-1);
-	i = 0;
-	last_cpy_index = 1;
-	while (++i < end_quote)
-	{
-		if (str[i] == '$' && i + 1 < end_quote)
-		{
-			push_str_part(parts,
-				ft_strncpy(str + last_cpy_index, i - last_cpy_index));
-			push_str_part(parts, get_env_variable(env, str + i));
-			i += ft_strchr_nalphanum(str + i + 1);
-			last_cpy_index = i + 1;
-		}
-	}
-	if (i - last_cpy_index > 0)
-		push_str_part(parts,
-			ft_strncpy(str + last_cpy_index, i - last_cpy_index));
-	return (end_quote);
-}
-
-static int	rem_single_quotes(t_string_part **parts, char *str)
-{
-	int	end_quote;
-
-	end_quote = is_quote_closed(str);
-	if (end_quote <= 1)
-		return (-1);
-	push_str_part(parts, ft_strncpy(str + 1, end_quote - 1));
-	return (end_quote);
-}
-
 static void	parse_word(t_string_part **parts, char *str, t_env *env)
 {
 	char	*to_put;
@@ -93,6 +53,29 @@ static void	parse_word(t_string_part **parts, char *str, t_env *env)
 			ft_strncpy(to_put + last_cpy_index, i - last_cpy_index));
 	free(to_put);
 }
+
+static int	rem_double_quotes(t_string_part **parts, char *str, t_env *env)
+{
+	int	end_quote;
+
+	end_quote = is_quote_closed(str);
+	if (!end_quote)
+		return (-1);
+	parse_word(parts, str + 1, env);
+	return (end_quote);
+}
+
+static int	rem_single_quotes(t_string_part **parts, char *str)
+{
+	int	end_quote;
+
+	end_quote = is_quote_closed(str);
+	if (end_quote <= 1)
+		return (-1);
+	push_str_part(parts, ft_strncpy(str + 1, end_quote - 1));
+	return (end_quote);
+}
+
 
 char	*rem_quotes(char *str, t_env *env)
 {
