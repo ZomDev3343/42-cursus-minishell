2891540b743ef506e0759f2dba6537d5f752c8e6
Florent Belotti <florent.l.d.belotti@gmail.com>
2024-05-30 01:11:47 +0200

    export re-work with exit code

--- src/builtin/ft_export.c
+++ src/builtin/ft_export.c
@@ -3,64 +3,72 @@
 /*                                                        :::      ::::::::   */
 /*   ft_export.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fbelotti <marvin@42perpignan.fr>           +#+  +:+       +#+        */
+/*   By: fbelotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 15:04:58 by fbelotti          #+#    #+#             */
-/*   Updated: 2024/05/29 17:12:21 by fbelotti         ###   ########.fr       */
+/*   Updated: 2024/05/30 01:09:05 by fbelotti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	is_valid_identifier(const char *str)
+void	check_export_operator(char *str)
 {
 	int	i;
 
 	i = 0;
-	if (!str || !str[0] || ft_isdigit(str[0]) || str[0] == '=')
-		return (0);
-	while (str[i])
-	{
-		if (!ft_isalphanum_c(str[i]) && str[i] != '_')
-			return (0);
+	while (str[i] && str[i] != '=')
 		i++;
+	if (str[i - 1] == '-' || i < 1)
+	{
+		perror("not a valid identifier\n");
+		exit(1);
 	}
-	return (1);
 }
 
 int	ft_strchr(char *str, char c)
 {
 	int	i;
 
+	i = -1;
+	while (str[++i])
+		if (str[i] == c)
+			return (TRUE);
+	return (FALSE);
+}
+
+int	is_only_digit(char *str)
+{
+	int	i;
+
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == c)
-			return (0);
+		if (!ft_isdigit(str[i]))
+			return (FALSE);
 		i++;
 	}
-	return (1);
+	return (TRUE);
 }
 
-void	check_identifier(char *identifier)
-{
-	if (!is_valid_identifier(identifier))
-	{
-		errno = EINVAL;
-		perror("export: not a valid identifier");
-		free(identifier);
-		exit(1);
-	}
-}
-
-void	check_args(t_command *cmd)
+void	check_export_arg(t_command *cmd)
 {
 	if (!cmd->parts[1])
 	{
-		errno = EINVAL;
-		perror(" not a valid identifier");
-		exit(1);
+		perror("not enough arguments\n");
+		exit(0);
 	}
+	if (ft_strchr(cmd->parts[1], '=') == FALSE)
+	{
+		perror("not a valid identifier\n");
+		if (ft_strchr(cmd->parts[1], '-') == TRUE)
+			exit(1);
+		if (is_only_digit(cmd->parts[1]) == TRUE)
+			exit(1);
+		exit(0);
+	}
+	else
+		check_export_operator(cmd->parts[1]);
 }
 
 void	ft_export(t_command *cmd, t_env *env)
@@ -68,25 +76,18 @@ void	ft_export(t_command *cmd, t_env *env)
 	t_env	*current_env;
 	char	**new_var;
 
-	check_args(cmd);
-	if (ft_strchr(cmd->parts[1], '=') == 0)
+	check_export_arg(cmd);
+	new_var = ft_split(cmd->parts[1], '=');
+	current_env = env;
+	while (current_env)
 	{
-		new_var = ft_split(cmd->parts[1], '=');
-		check_identifier(new_var[0]);
-		current_env = env;
-		while (current_env)
-		{
-			if (ft_strcmp(current_env->name, new_var[0]) == 0)
-				remove_env_var(env, current_env->name);
-			current_env = current_env->next;
-		}
-		add_env_variable(env, newenv(ft_strdup(new_var[0]),
-				ft_strdup(new_var[1])));
-		free_parts(new_var);
+		if (ft_strncmp(current_env->name, new_var[0], ft_strlen(new_var[0])))
+			remove_env_var(env, current_env->name);
+		current_env = current_env->next;
 	}
-	else
-	{
-		check_identifier(cmd->parts[1]);
-		add_env_variable(env, newenv(ft_strdup(cmd->parts[1]), NULL));
-	}
+	add_env_variable(env, newenv(ft_strcpy(new_var[0]), ft_strcpy(new_var[1])));
+	free_parts(new_var);
 }
+
+
+
